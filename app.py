3d6d08255e1d766369a2fb4c5c2cc9015269a240
Helen Quine <hkquine@gmail.com>
2020-03-25 17:36:59 -0700

    End of day one. Stage 1 basically completed. (default image and testing still to do)

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Blogly application."""
 
-from flask import Flask
+from flask import Flask, redirect, render_template, request
 from models import db, connect_db, User
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -15,14 +15,26 @@ debug = DebugToolbarExtension(app)
 connect_db(app)
 db.create_all()
 
-@app.route("/"):
+@app.route("/")
+def home_page():
+  """ redirects to users """
+
+  return redirect("/users")
+
+@app.route("/users")
 def list_users():
-    """ list users and show a button to add a user"""
+  """ list users and show a button to add a user"""
 
   users = User.query.all()
   return render_template("userlist.html", users = users)
 
-@app.route("/newuser", methods =["POST"])
+@app.route("/users/new")
+def new_user_form():
+  """ renders the new user form page"""
+
+  return render_template("newuser.html")
+
+@app.route("/users/new", methods =["POST"])
 def add_user():
   """Add user and redirect to list."""
 
@@ -34,12 +46,48 @@ def add_user():
   db.session.add(user)
   db.session.commit()
 
-  return redirect("/userlist")
+  return redirect("/users")
 
-@app.route("/userdetail/<int:user_id>"):
-def show_userdetail():
+@app.route("/users/<int:user_id>")
+def show_userdetail(user_id):
   """show details about user """
 
   user = User.query.get_or_404(user_id)
   return render_template("userdetail.html", user=user)
+
+@app.route("/users/<int:user_id>/edit")
+def show_user_edit_form(user_id):
+  """show form to edit user details"""
+
+  user = User.query.get_or_404(user_id)
+  return render_template("user_edit.html", user=user)
+
+@app.route("/users/<int:user_id>/edit", methods=["POST"])
+def edit_user(user_id):
+  """Commits user edits to db and redirects to list."""
+  
+  user = User.query.get_or_404(user_id)
+
+  user.first_name = request.form['first_name']
+  user.last_name = request.form['last_name']
+  user.image_url = request.form['image_url']
+
+  db.session.add(user) 
+  "But we don't need this?"
+  db.session.commit()
+
+  return redirect("/users")
+
+@app.route("/users/<int:user_id>/delete", methods=["POST"])
+def delete_user(user_id):
+  """Delete user form the db"""
+
+  user = User.query.get_or_404(user_id)
+
+  db.session.delete(user)
+  db.session.commit()
+
+  return redirect("/users")
+
+
 
--- models.py
+++ models.py
@@ -8,7 +8,7 @@ def connect_db(app):
   """Connect to database."""
 
   db.app = app
-  db.initi_app(app)
+  db.init_app(app)
 
 class User(db.Model):
   """ User."""
@@ -18,4 +18,4 @@ class User(db.Model):
   id = db.Column(db.Integer, primary_key = True, autoincrement = True)
   first_name = db.Column(db.String(50), nullable = False)
   last_name = db.Column(db.String(50), nullable = False)
-  image_url = db.Column(db.String(), nullabe = False)
+  image_url = db.Column(db.String(), nullable = False)
